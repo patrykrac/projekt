022a4b421968601ec490e0e42c91375600539f4c
Jelon <86931022+Jelon1211@users.noreply.github.com>
2024-03-11 22:39:24 +0100

    Update 1.py
    Podstawy do poprawy :D

--- 1.py
+++ 1.py
@@ -3,16 +3,17 @@ import json
 
 kuplubsprze = input("Chcesz kupić czy sprzedać: ")
 waluta = input("jaką walutę: ")
-
+# Rozdziel logikę pobierania kursów walut, interakcję z użytkownikiem i obliczenia na oddzielne funkcje.
 response = requests.get("http://api.nbp.pl/api/exchangerates/rates/A/USD/")
 rusd = response.json()["rates"]
 usd = float(rusd[0]["mid"])
-response = requests.get("http://api.nbp.pl/api/exchangerates/rates/A/EUR/")
+response = requests.get("http://api.nbp.pl/api/exchangerates/rates/A/EUR/") #Użyj stałych dla URLi i lepszych nazw zmiennych.
 reur = response.json()["rates"]
 eur = float(reur[0]["mid"])
 response = requests.get("http://api.nbp.pl/api/exchangerates/rates/A/GBP/")
 rgbp = response.json()["rates"]
 gbp = float(rgbp[0]["mid"])
+# Dodaj obsługę błędów dla żądań HTTP i konwersji danych.
 
 if kuplubsprze == "kupić":
     hajs = float(input("za ile chcesz kupić: "))
@@ -32,3 +33,4 @@ elif kuplubsprze == "sprzedać":
     elif waluta == "gbp":
         print(f"dostaniesz {round(hajs*gbp, 2)} zł, więc na browara może wystarczy")
     else: print("coś zepsułeś")
+# Zminimalizuj powtarzanie kodu przez wykorzystanie funkcji.
