97fa855dbb720f2630ff39f711559facf2963dfe
patrykrac <patryk.jedrusiak92@gmail.com>
2024-03-12 19:15:24 +0100

    ćwiczenie

---
+++ waga_na_planetach.py
@@ -0,0 +1,19 @@
+waga = float(input("podaj swoją wagę: "))
+planeta = input("podaj planetę gdzie chciałbyś się znaleźć: ").capitalize()
+if planeta == "Merkury":
+    print(f"Twoja waga tam to {waga*0.378}")
+elif planeta == "Wenus":
+    print(f"Tam ważysz {waga * 0.907}")
+elif planeta == "Mars":
+    print(f"Tam ważysz {waga * 0.377}")
+elif planeta == "Jowisz":
+    print(f"Tam ważysz {waga * 2.528}")
+elif planeta == "Saturn":
+    print(f"Tam ważysz {waga * 1.064}")
+elif planeta == "Uran":
+    print(f"Tam ważysz {waga * 0.889}")
+elif planeta == "Neptun":
+    print(f"Tam ważysz {waga * 1.125}")
+elif planeta == "Księżyc":
+    print(f"to nie planeta... ale jak już pytasz to tam ważysz{waga * 0.166}")
+else: print("w naszym układzie planetarnym nie ma takiej planety")
